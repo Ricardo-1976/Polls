3b0292100d7c644f572151389de567b9c87e60e3
Ricardo João António <ricardojoaoantonio2@gmail.com>
2024-09-29 01:34:59 +0100

    fist

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,30 @@
+import type { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+import { title } from "process";
+
+export async function getPoll(app: FastifyInstance) {
+  app.get('/polls/:pollId', async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid(),
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId,
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+          }
+        }
+      }
+    })
+
+    return reply.send({ poll })
+  })
+}
